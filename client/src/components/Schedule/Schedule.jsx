e6a50d3e8c6a0edf95ad65a73662468e4d82fa27
Cameron Lopez <kamrin717@gmail.com>
2025-10-20 23:18:47 -0500

    almost done with volunteer view

--- client/src/components/Schedule/Schedule.jsx
+++ client/src/components/Schedule/Schedule.jsx
@@ -1,7 +1,69 @@
-import React from "react";
+import styles from "./Schedule.module.css";
+import React, { useEffect, useState } from "react";
+import { getSundaysInMonth, formatDate } from "../../utils/Helpers";
 
 const Schedule = ({ team }) => {
-  return <div>{team.name}</div>;
+  const [schedule, setSchedule] = useState([]);
+  const today = new Date();
+  const currentMonth = today.getMonth(); // 0-11
+  const currentYear = today.getFullYear();
+
+  const sundays = getSundaysInMonth(currentYear, currentMonth).map((d) =>
+    d.toLocaleDateString()
+  );
+
+  useEffect(() => {
+    const fetchSchedule = async () => {
+      if (!team || !team.id) {
+        setSchedule([]);
+        return;
+      }
+      const response = await fetch(`/api/read/get_schedule/${team.id}`);
+      const data = await response.json();
+      if (data.success) {
+        setSchedule(data.schedule);
+      } else {
+        setSchedule([]);
+      }
+    };
+    fetchSchedule();
+  }, [team]);
+
+  if (team === undefined) {
+    return (
+      <p style={{ marginTop: "2rem", fontStyle: "italic" }}>
+        Please select a team to view the schedule.
+      </p>
+    );
+  } else if (schedule?.length === 0) {
+    return (
+      <p style={{ marginTop: "2rem", fontStyle: "italic" }}>
+        Team Lead has not scheduled for this month yet.
+      </p>
+    );
+  }
+  return (
+    <div className={styles.volunteerViewContainer}>
+      {sundays.map((date, index) => (
+        <div className={styles.dateCard} key={index}>
+          <h3>{date}</h3>
+          <ul>
+            {schedule?.filter(
+              (s) => formatDate(s.scheduled_date) === formatDate(date)
+            ).length === 0 ? (
+              <li>No volunteers assigned</li>
+            ) : (
+              schedule
+                ?.filter(
+                  (s) => formatDate(s.scheduled_date) === formatDate(date)
+                )
+                .map((s, idx) => <li key={idx}>{s.volunteer_name}</li>)
+            )}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Schedule;
--- client/src/routes/Volunteer/VolunteerHome.jsx
+++ client/src/routes/Volunteer/VolunteerHome.jsx
@@ -14,16 +14,45 @@ const VolunteerHome = () => {
   const currentMonth = today.getMonth(); // 0-11
   const currentYear = today.getFullYear();
   const monthName = today.toLocaleString("default", { month: "long" });
+  const [teams, setTeams] = useState([]);
   const [team, setTeam] = useState({});
 
+  useEffect(() => {
+    const getTeams = async () => {
+      try {
+        const response = await fetch("/api/read/get_teams");
+        const data = await response.json();
+        if (!data.success) {
+          throw new Error(data.message || "Failed to fetch teams");
+        }
+        setTeams(data.teams);
+      } catch (error) {
+        toast.error(`Error fetching teams: ${error.message}`);
+        console.error(error);
+      }
+    };
+    getTeams();
+  }, []);
+
   return (
-    <>
+    <div className={styles.volunteerHomeContainer}>
       <h1>
         Schedules for {monthName} {currentYear}
       </h1>
-
+      <select
+        name="team_name"
+        id="team_name"
+        onChange={(e) => setTeam(teams.find((t) => t.name === e.target.value))}
+      >
+        <option value="">--Select a team--</option>
+        {TEAMS.map(({ val, label }, index) => (
+          <option value={val} key={index}>
+            {label}
+          </option>
+        ))}
+      </select>
       <Schedule team={team} />
-    </>
+    </div>
   );
 };
 
