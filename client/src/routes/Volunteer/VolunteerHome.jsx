3c276bb1fa8cb11a4a5563402fcf6cb2eced1934
Cameron Lopez <kamrin717@gmail.com>
2025-11-10 01:44:25 -0600

    started new branch

--- client/src/routes/Volunteer/VolunteerHome.jsx
+++ client/src/routes/Volunteer/VolunteerHome.jsx
@@ -16,6 +16,11 @@ const VolunteerHome = () => {
   const monthName = today.toLocaleString("default", { month: "long" });
   const [teams, setTeams] = useState([]);
   const [team, setTeam] = useState({});
+  const [schedule, setSchedule] = useState([]);
+
+  const sundays = getSundaysInMonth(currentYear, currentMonth).map((d) =>
+    d.toLocaleDateString()
+  );
 
   useEffect(() => {
     const getTeams = async () => {
@@ -34,6 +39,30 @@ const VolunteerHome = () => {
     getTeams();
   }, []);
 
+  const getSchedule = async (team_name) => {
+    if (team_name === "") return;
+    const team_id = teams.find((t) => t.name === team_name);
+    if (!team_id) {
+      toast.error("Team Schedule unavailable");
+      setSchedule([]);
+      return;
+    }
+    try {
+      const response = await fetch(`/api/read/get_schedule/${team_id.id}`);
+      const data = await response.json();
+      if (!data.success) throw new Error(data.message);
+      if (!data.schedule) {
+        toast.error("Team Schedule unavailable");
+        setSchedule([]);
+        return;
+      }
+      setSchedule(data.schedule);
+    } catch (error) {
+      console.error("[ERROR]: ", error);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className={styles.volunteerHomeContainer}>
       <h1>
@@ -42,7 +71,7 @@ const VolunteerHome = () => {
       <select
         name="team_name"
         id="team_name"
-        onChange={(e) => setTeam(teams.find((t) => t.name === e.target.value))}
+        onChange={(e) => getSchedule(e.target.value)}
       >
         <option value="">--Select a team--</option>
         {TEAMS.map(({ val, label }, index) => (
@@ -51,7 +80,29 @@ const VolunteerHome = () => {
           </option>
         ))}
       </select>
-      <Schedule team={team} />
+      <div className={styles.volunteerHomeScheduleBlock}>
+        {schedule?.length === 0 ? (
+          <p style={{ marginTop: "1rem" }}>Select a Team to get schedule</p>
+        ) : (
+          sundays.map((date, index) => (
+            <div
+              key={index}
+              className={styles.volunteerHomeScheduleSundayBlock}
+            >
+              <h3>{date}</h3>
+              <div>
+                {schedule
+                  ?.filter(
+                    (s) => formatDate(s.scheduled_date) === formatDate(date)
+                  )
+                  .map(({ id, volunteer_name }) => (
+                    <p key={id}>- {volunteer_name}</p>
+                  ))}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
